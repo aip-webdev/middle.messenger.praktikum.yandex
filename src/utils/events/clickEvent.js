0418e25e9bb7693e63721dfbb2e87d2b415598ca
Алексей Петренко <aipetrenko@corp.finam.ru>
2023-06-17 23:44:19 +0600

    - добавлен шобланизатор - в роутер добавлена логика, благодяря которой не требующие изменения элементы не изменяются при изменении url - оптимизированы стили

--- src/utils/events/clickEvent.js
+++ src/utils/events/clickEvent.js
@@ -13,4 +13,3 @@ export const clickEvent = (id, handler) => waitElement(id).then((element) =>
         }
     })
 )
-
--- src/utils/stringToColor.js
+++ src/utils/stringToColor.js
@@ -1,17 +1,17 @@
 export const stringToColor = (string) => {
     let hash = 0;
-    let i;
+    let i
 
     for (i = 0; i < string.length; i += 1) {
-        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        hash = string.charCodeAt(i) + ((hash << 5) - hash)
     }
 
-    let color = '#';
+    let color = '#'
 
     for (i = 0; i < 3; i += 1) {
         const value = (hash >> (i * 8)) & 0xff;
-        color += `00${value.toString(16)}`.substr(-2);
+        color += `00${value.toString(16)}`.substr(-2)
     }
 
-    return color.toString();
-};
+    return color.toString()
+}
